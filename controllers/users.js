8439998529230bbe67cbe0a73b728cda8013091b
Eviassor789 <102507977+Eviassor789@users.noreply.github.com>
2023-06-01 23:31:54 +0300

    add user

--- controllers/users.js
+++ controllers/users.js
@@ -10,25 +10,28 @@ function getUserDetails(req, res) {
     // var json_answer = getUser(req.params.username);
     // the getUser() function is a model function which returns the json of username, displayName and profilePic.
     // if(json_answer == null) {
-    //    res.status(401);
-    //    res.title("unauthorized")
-    //    return res.json();
+    //    return res.status(401).json({"title": "there is no such user"});
     //}
     return res.status(200).json(
         //json_answer
     );
   } catch (error) {
-    res.status(500).json({
-      message: "An error occurred while adding the user",
-      error: error.message,
-    });
+    res.status(500).send("An error occurred while adding the user");
   }
 }
 
 function register(req, res) {
-
-
-    return res.json();
+    try{
+        if(req.body.username && req.body.password && req.body.displayName && req.body.profilePic) {
+            //addUser(req.body.username, req.body.password, req.body.displayName, req.body.profilePic);
+            //addUser adds a new user in the database, implement it in model.
+            return res.status(200).json();
+        } else {
+            return res.status(409).json({"title": "conflict"});
+        }
+    } catch (error) {
+        res.status(500).send("An error occurred while adding the user");
+      }
 }
 
 export {getUserDetails, register}
